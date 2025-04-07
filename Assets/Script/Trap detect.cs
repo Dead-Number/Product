d8f7c96c359d61c20d5cf1404afc86c2aab72ca7
Jules PECH <julespech@gmail.com>
2025-04-07 11:23:11 +0200

    Trap working !!

--- Assets/Script/Trap detect.cs
+++ Assets/Script/Trap detect.cs
@@ -1,19 +1,40 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class Trapdetect : MonoBehaviour
 {
 
-    public LayerMask _layermask;
+    public float posFinal = -7.1f;
+    public float posInit = -5.25f;
+    public float _duration = 0.5f;
+
+    public Ease AnimType;
+
 
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _layermask);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, Mathf.Infinity, LayerMask.GetMask("Player"));
 
         if (hit)
         {
-            Debug.Log("CRASH");
+            StartCoroutine(TrapCicle());
         }
+    }
+
+    IEnumerator TrapCicle()
+    {
+        Debug.Log("YESSSSS");
+        
+        transform.DOMoveY(posFinal, _duration)
+            .SetEase(AnimType);
+
+        yield return new WaitForSeconds(5);
+
+        transform.DOMoveY(posInit, _duration)
+            .SetEase(AnimType);
+
     }
 }
